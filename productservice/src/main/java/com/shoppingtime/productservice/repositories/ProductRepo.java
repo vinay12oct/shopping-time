96fd8d5a8a7283c5edabf4adab80fff2129d41da
Mr V Patel <ivinaypatelemail@gmail.com>
2024-09-26 00:38:24 +0530

    Add two new method in Product repository

--- productservice/src/main/java/com/shoppingtime/productservice/repositories/ProductRepo.java
+++ productservice/src/main/java/com/shoppingtime/productservice/repositories/ProductRepo.java
@@ -1,9 +1,14 @@
 package com.shoppingtime.productservice.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shoppingtime.productservice.entities.Product;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
+	
+	List<Product> findByCategory(String category);
+	List<Product> findByBrand(String brand);
 
 }
